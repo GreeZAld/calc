8898a24b1b97cb24b82d38f51c27fdd94c7ff187
Oleg Tarasevich <cantbekind@gmail.com>
2018-05-13 13:01:48 +0300

    1 handleChange, only numbers

--- src/components/input.js
+++ src/components/input.js
@@ -10,22 +10,15 @@ class Input extends Component {
       option: '',
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChange1 = this.handleChange1.bind(this);
-    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.calculateSum = this.calculateSum.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleChange1(event) {
-    this.setState({month: event.target.value});
-  }
-
-  handleChange2(event) {
-    this.setState({option: event.target.value});
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({[name]: value});
   }
 
   calculateSum = () => {
@@ -34,8 +27,6 @@ class Input extends Component {
       (parseFloat(this.state.value) * (this.state.option / 12) / 100) *
       this.state.month,
     });
-    var sum = this.summ;
-    return sum;
   };
 
   render() {
@@ -45,17 +36,17 @@ class Input extends Component {
     const option = this.state.option;
     return (
         <div>
-          <h3>Выберите тип кредита: <select name="loan" value={option}
-                                            onChange={this.handleChange2}>
+          <h3>Выберите тип кредита: <select name="option" value={option}
+                                            onChange={this.handleInputChange}>
             <option value="">---</option>
             <option value="3">для бедных</option>
             <option value="10">для обычных</option>
             <option value="0.5">для умных</option>
           </select></h3>
-          <h3>Сумма кредита: <input type="text" id="money" value={value}
-                                    onChange={this.handleChange}/> р.</h3>
-          <h3>Срок кредитования: <input type="text" id="term" value={month}
-                                        onChange={this.handleChange1}
+          <h3>Сумма кредита: <input type="number" id="money" name="value" value={value}
+                                    onChange={this.handleInputChange}/> р.</h3>
+          <h3>Срок кредитования: <input type="number" id="term" name="month" value={month}
+                                        onChange={this.handleInputChange}
                                         onKeyUp={this.calculateSum}/> мес.</h3>
           <hr/>
           <h3>Сумма к выплате: <text id="out">{summ}</text> р.</h3>
